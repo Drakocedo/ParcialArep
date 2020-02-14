3d03225150dd02531f097c6c44898863d3a0bcbb
unknown <david.caycedo@labinfo.is.escuelaing.edu.co>
2020-02-14 10:51:32 -0500

    proyecto completo

--- ParcialArep/src/main/java/edu/escuelaing/arep/app/SparkWeb.java
+++ ParcialArep/src/main/java/edu/escuelaing/arep/app/SparkWeb.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package edu.escuelaing.arep.app;
+import static spark.Spark.*;
 
 /**
  *
  * @author david.caycedo
  */
 public class SparkWeb {
+    public static void main(String[] args) {
+        port(getPort());
+        get("/", (req, res) -> "Hello Heroku");
+ }
+    
+    
+    static int getPort() {
+    if (System.getenv("PORT") != null) {
+    return Integer.parseInt(System.getenv("PORT"));
+    }
+    return 4567; //returns default port if heroku-port isn't set
+   
+    }
+    
     
 }
